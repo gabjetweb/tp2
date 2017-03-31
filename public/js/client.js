93a32383409870b7f64cd26ff9222b6eaf665f58
gabjetté <gabjet.web@gmail.com>
2017-03-31 19:36:21 -0400

    modification de la structure des documents

---
+++ public/js/client.js
@@ -0,0 +1,18 @@
+
+function modifier_adresse(elm){
+
+        var elmLigne = elm.parentNode.parentNode; //association à son élément parent : <tr>
+
+        //envoie des informations HTML de l'élément vers le formulaire d'ajout
+          document.getElementById("nom").value = elmLigne.children[0].innerHTML;
+          document.getElementById("prenom").value = elmLigne.children[1].innerHTML;
+          document.getElementById("telephone").value = elmLigne.children[2].innerHTML;
+          document.getElementById("ville").value = elmLigne.children[3].innerHTML;
+          document.getElementById("codePostal").value = elmLigne.children[4].innerHTML;
+          document.getElementById("_id").value = elmLigne.children[5].innerHTML;
+
+        //Soumission et envoie
+        var elmForm = document.getElementById("formulaire");
+        elmForm.submit();
+
+}
